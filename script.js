09b9026bb73b25758004bf983cf59deb61fa8dea
Vadim <belyaev141083@yandex.ru>
2022-05-18 03:07:51 +0500

    upgrade2

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var chkBox = document.getElementById('nav-toggle');
-    chkBox.onchange = function(){
-        if(chkBox.checked){
-            chkBox.parentNode.style.backgroundColor = "#FFF";
-        } else{
-            chkBox.parentNode.style.backgroundColor = "";
-        }
-    };
+// We enable .scrollline-text when full load page
+export const categoryAnimate = () => {
+    window.addEventListener('load', (event) => {
+        var elements = document.querySelectorAll('.animation-text');
+        for (var i = 0; i < elements.length; i++)
+            elements[i].style.display = 'block';
+    });
+}
